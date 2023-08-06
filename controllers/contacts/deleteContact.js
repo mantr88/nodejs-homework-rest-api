bd2ccfb2108d2568fbe58db79aed0a4f857935b6
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-08-06 17:03:35 +0300

    update delete contact controller

--- controllers/contacts/deleteContact.js
+++ controllers/contacts/deleteContact.js
@@ -1,16 +1,17 @@
+const asyncHandler = require("express-async-handler");
 const Contact = require("../../models/contacts/index");
 
-const deleteContact = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await Contact.findByIdAndRemove(contactId);
-    if (!contact) {
-      return res.status(404).json({ message: "Not found" });
-    }
-    return res.status(200).json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
+const deleteContact = asyncHandler(async (req, res) => {
+  const { contactId } = req.params;
+  console.log(contactId);
+  const contact = await Contact.findByIdAndRemove(contactId);
+  console.log("1");
+  console.log(contact);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Not found");
   }
-};
+  return res.status(200).json({ message: "contact deleted" });
+});
 
 module.exports = deleteContact;
